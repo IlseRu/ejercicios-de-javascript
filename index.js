7efbf119b8dd4305f946ceda8d379205667fd700
Ilse Ruiz <ada_hack146@hackademy.mx>
2022-03-13 01:38:17 -0600

    Ejercicio 2
    Se crea ejercicio 2 de suma de array

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ const sumar = (x,y) => {
     return console.log(x+y);
 }
 
+console.log("Ejercicio 1");
 sumar(2,3);
+
+//////////////////////////////
+
+
+let array= [1,2,3,4,5,6,7,8,9,10];
+let sumando=0;
+
+console.log(array);
+
+const sumarArreglos = (array) =>{
+
+    for(let i = 0 ; i < array.length; i++){
+        sumando = sumando + array[i];
+        console.log("Sumando en el for: " + sumando);
+
+    }
+    console.log("total: " + sumando)
+}
+
+console.log("Ejercicio 2")
+sumarArreglos(array);
